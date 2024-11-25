d1e1a6d47a0073c0e4d08ba1fd3f4de1b567e958
regishase <135616411+regishase@users.noreply.github.com>
2024-11-24 21:42:44 -0300

    Add files via upload

---
+++ thisEbind.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    saudacao: "Bom Dia!",
+    falar() {
+        console.log(this.saudacao)
+    }
+}
+
+pessoa.falar()
+
+const falar = pessoa.falar
+falar()
+
+const falarDePessoa = pessoa.falar.bind(saudacao)
+falarDePessoa()
